c1117621824c6669de9a6af25a0b63251a3c3db4
yue <zhyue@umich.edu>
2017-09-28 01:01:48 -0700

    init project

---
+++ app/src/main/java/com/laioffer/hotevents/DataService.java
@@ -0,0 +1,24 @@
+package com.laioffer.hotevents;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yue on 5/16/17.
+ */
+
+public class DataService {
+    /**
+     * Fake all the event data for now. We will refine this and connect
+     * to our backend later.
+     */
+    public static List<Event> getEventData() {
+        List<Event> eventData = new ArrayList<Event>();
+        for (int i = 0; i < 10; ++i) {
+            eventData.add(
+                    new Event());
+        }
+        return eventData;
+    }
+
+}
